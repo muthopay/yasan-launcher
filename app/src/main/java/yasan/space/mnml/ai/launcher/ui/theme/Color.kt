8d8b3f36b8bee9dd23be355ee3aa04d29009f241
Yasan Ghafariyan <yasandev@gmail.com>
2021-03-29 15:25:52 +0430

    Import YASAN brand colors

--- app/src/main/java/yasan/space/mnml/ai/launcher/ui/theme/Color.kt
+++ app/src/main/java/yasan/space/mnml/ai/launcher/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package yasan.space.mnml.ai.launcher.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val YasanBlueLight = Color(0xFF5BDFE6)
+val YasanBlue = Color(0xFF00ADB4)
+val YasanBlueDark = Color(0xFF007D84)
+
+val YasanOrangeLight = Color(0xFFFF8951)
+val YasanOrange = Color(0xFFFF5623)
+val YasanOrangeDark = Color(0xFFC41B00)
