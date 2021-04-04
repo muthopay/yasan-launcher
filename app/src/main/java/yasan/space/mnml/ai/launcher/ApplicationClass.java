dd926eeb6cd44c3909b463142d8f301527a60f5b
YASAN <yasandev@gmail.com>
2021-04-05 00:43:49 +0430

    Create ApplicationClass

---
+++ app/src/main/java/yasan/space/mnml/ai/launcher/ApplicationClass.java
@@ -0,0 +1,9 @@
+package yasan.space.mnml.ai.launcher;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ApplicationClass extends Application {
+}
