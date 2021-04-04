495e88d1b04fb8d06e7c5b61079fee6063caf550
YASAN <yasandev@gmail.com>
2021-04-05 00:44:20 +0430

    Update AppModule.kt

--- app/src/main/java/yasan/space/mnml/ai/launcher/di/AppModule.kt
+++ app/src/main/java/yasan/space/mnml/ai/launcher/di/AppModule.kt
@@ -1,16 +1,24 @@
 package yasan.space.mnml.ai.launcher.di
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dev.yasan.helper.library.getMySharedPreferences
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import yasan.space.mnml.ai.launcher.data.app.AppDatabase
+import yasan.space.mnml.ai.launcher.data.app.AppRepository
+import yasan.space.mnml.ai.launcher.data.app.DefaultAppRepository
+import yasan.space.mnml.ai.launcher.data.settings.DefaultSettingsRepository
+import yasan.space.mnml.ai.launcher.data.settings.SettingsRepository
 import yasan.space.mnml.ai.launcher.util.ApplicationScope
 import yasan.space.mnml.ai.launcher.util.DispatcherProvider
 import java.util.*
@@ -37,10 +45,28 @@ class AppModule {
     @Provides
     fun provideAppDao(db: AppDatabase) = db.appDao()
 
+    @Singleton
+    @Provides
+    fun provideAppRepository(
+        appDatabase: AppDatabase,
+        settingsRepo: SettingsRepository
+    ): AppRepository =
+        DefaultAppRepository(appDatabase, settingsRepo)
+
     @ApplicationScope
     @Provides
     @Singleton
     fun provideApplicationScope() = CoroutineScope(SupervisorJob())
+
+    @Singleton
+    @Provides
+    fun provideSettingsRepository(sp: SharedPreferences): SettingsRepository =
+        DefaultSettingsRepository(sp)
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences =
+        context.getMySharedPreferences()
 
     @Singleton
     @Provides
