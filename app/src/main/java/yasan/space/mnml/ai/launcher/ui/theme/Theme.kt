7fc33d1f8f9c4b14e22053f499ff10b10794c4d8
YASAN <yasandev@gmail.com>
2021-04-13 20:23:39 +0430

    Fix theme name

--- app/src/main/java/yasan/space/mnml/ai/launcher/ui/theme/Theme.kt
+++ app/src/main/java/yasan/space/mnml/ai/launcher/ui/theme/Theme.kt
@@ -29,7 +29,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun YASANLauncherOTheme(
+fun YASANLauncherTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
