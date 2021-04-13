e18c11a1b4fd3b85095cb204c3923ef7c0f5bd8a
YASAN <yasandev@gmail.com>
2021-04-14 01:24:06 +0430

    Load app images on Home

--- app/src/main/java/yasan/space/mnml/ai/launcher/ui/app/AppHome.kt
+++ app/src/main/java/yasan/space/mnml/ai/launcher/ui/app/AppHome.kt
@@ -1,16 +1,15 @@
 package yasan.space.mnml.ai.launcher.ui.app
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.imageloading.toPainter
 import yasan.space.mnml.ai.launcher.MainActivity
 import yasan.space.mnml.ai.launcher.data.app.App
 
@@ -19,17 +18,19 @@ fun AppHome(
     app: App,
     activity: MainActivity
 ) {
-    Column() {
-        Text(
-            text = app.label,
-            modifier = Modifier
-                .width(128.dp)
-                .background(Color.White)
-                .clickable {
-                    app.launch(activity)
-                }
-                .padding(16.dp),
-            textAlign = TextAlign.Center)
-    }
+    val drawable = app.requireIcon(activity)!!
+    Image(
+        painter = drawable.toPainter(),
+        contentDescription = app.label,
+        modifier = Modifier
+            .width(64.dp)
+            .height(64.dp)
+            .padding(12.dp)
+            .clickable {
+                app.launch(activity)
+            },
+        alignment = Alignment.Center
+    )
+
 
 }
