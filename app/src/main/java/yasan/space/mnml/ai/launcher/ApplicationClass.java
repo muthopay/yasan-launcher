b76a35086a8f5975c65fffcf9189676c9137110b
YASAN <yasandev@gmail.com>
2021-04-15 02:53:22 +0430

    Convert ApplicationClass to Kotlin

--- app/src/main/java/yasan/space/mnml/ai/launcher/ApplicationClass.java
+++
@@ -1,9 +0,0 @@
-package yasan.space.mnml.ai.launcher;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class ApplicationClass extends Application {
-}
---
+++ app/src/main/java/yasan/space/mnml/ai/launcher/ApplicationClass.kt
@@ -0,0 +1,7 @@
+package yasan.space.mnml.ai.launcher
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationClass : Application()
